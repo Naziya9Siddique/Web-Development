f3e474e139f2990045dc9551c3197fd7549b4f20
Naziya9Siddique <112797466+Naziya9Siddique@users.noreply.github.com>
2024-02-05 02:04:37 +0530

    Add files via upload

---
+++ traffic.js
@@ -0,0 +1,10 @@
+let color="yellow";
+if(color==="red"){
+    console.log("Stop! Light is red.");
+}
+if(color==="yellow"){
+    console.log("Slow Down. Light is yellow.");
+}
+if(color==="green"){
+    console.log("Go. Light is green.");
+}
