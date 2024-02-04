9eb9e27d2baeef7cd59ef20209bca7b5a88f8f4a
Naziya9Siddique <112797466+Naziya9Siddique@users.noreply.github.com>
2024-02-05 02:19:43 +0530

    Add files via upload

---
+++ elseif.js
@@ -0,0 +1,12 @@
+let month="july";
+if(month==="january"){
+    console.log("Winter is here");
+}
+else if(month==="april")
+{
+    console.log("Summer is here.");
+}
+else if(month==="july")
+{
+    console.log("Rainy is here");
+}
