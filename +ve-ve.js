638b453e84181144aed5d46055be252561a0ff08
Naziya9Siddique <112797466+Naziya9Siddique@users.noreply.github.com>
2024-02-05 02:50:32 +0530

    Add files via upload

---
+++ +ve-ve.js
@@ -0,0 +1,8 @@
+//Write a program that determines whether a given number is positive or negative.
+let num=-23;
+if(num>0){
+    console.log("Positive Number");
+}
+else{
+    console.log("Negative Number");
+}
