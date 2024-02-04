70c2677224e4189f4034314e914be4e42b8e599b
Naziya9Siddique <112797466+Naziya9Siddique@users.noreply.github.com>
2024-02-05 03:10:29 +0530

    Add files via upload

---
+++ ticket.js
@@ -0,0 +1,17 @@
+//Write a program that calculates the ticket price based on age with the following conditions:
+// age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price
+// of 20, age over 60 play a ticket price of 15.
+
+let age=43;
+if(age<12){
+    console.log("Ticket Price is 5.");
+}
+else if(age<18){
+    console.log("Ticket Price is 10.");
+}
+else if(age<60){
+    console.log("Ticket Price is 20.");
+}
+else{
+    console.log("Ticket Price is 15.")
+}
