cfd5193b642047316385fba6e0c149e7cd3632a3
Naziya9Siddique <112797466+Naziya9Siddique@users.noreply.github.com>
2024-02-05 02:55:04 +0530

    Add files via upload

---
+++ greater.js
@@ -0,0 +1,9 @@
+//Write a program to determine the greater of two numbers.
+let num1=89;
+let num2=870;
+if(num1>num2){
+    console.log("Num1 is greater ",num1);
+}
+else{
+    console.log("Num2 is greater ",num2);
+}
