47f1b4ed4cd0eee3f19dc1e9272f804eee5267f4
Naziya9Siddique <112797466+Naziya9Siddique@users.noreply.github.com>
2024-02-05 03:14:25 +0530

    Add files via upload

---
+++ year.js
@@ -0,0 +1,9 @@
+//Write a program that determines if a year is a leap year.
+
+let year=2034;
+if(year%4===0){
+    console.log(year,"is leap year.")
+}
+else{
+    console.log(year,"is non leap year.")
+}
